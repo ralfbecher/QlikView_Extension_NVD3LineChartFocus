636c1ee4ccdbf56c004e8dd9734f33406cf15ad5
Ralf Becher <ralf.becher@tiq-solutions.de>
2014-09-26 17:21:29 +0200

    fixes server site requirejs loading

--- NVD3LineChartFocus/script.js
+++ NVD3LineChartFocus/script.js
@@ -14,92 +14,96 @@ if(!window.console){ window.console = {log: function(){} }; }
 (function ($) {
 	//own context, avoiding conflicts with other libraries, $=jQuery
 	var _extension = 'NVD3LineChartFocus';
-    var _path = 'Extensions/' + _extension + '/';
-	var _pathLong = Qva.Remote + (Qva.Remote.indexOf('?') >= 0 ? '&' : '?') + 'public=only&name=' + _path;
+    var _pathShort = 'Extensions/' + _extension + '/';
+	var _pathLong = Qva.Remote + (Qva.Remote.indexOf('?') >= 0 ? '&' : '?') + 'public=only&name=' + _pathShort;
 	// detect WebView mode (QlikView Desktop)
 	var _webview = window.location.host === 'qlikview';
+	var _path = (_webview ? _pathShort : _pathLong);
 
 	// load all libraries as array, don't use nested Qva.LoadScript() calls
 	Qv.LoadExtensionScripts([_path + 'js/require.js'], 
 		function () {
-			require([_path + 'js/d3.v3', _path + 'js/nv.d3.min', _path + 'js/interactiveLayer', _path + 'js/utils'], 
+			require([_path + 'js/d3.v3.js'], 
 				function () {
-					Qv.AddExtension(_extension,
+					Qv.LoadExtensionScripts([_path + 'js/nv.d3.min.js', _path + 'js/interactiveLayer.js', _path + 'js/utils.j'], 
 						function () {
-							var showOthers = ((this.Layout.Text0.text.toString() * 1) > 0);
-							// load css file
-							Qva.LoadCSS((_webview ? _path : _pathLong) + 'css/nv.d3.min.css');
-							
-							// need a unique id to render chart
-							var objId = this.Layout.ObjectId.replace("\\", "_"); // chart name in CSS class (eg "QvFrame Document_CH01")
-
-							//console.log(objId);
-							if (this.Data.Rows.length > 0) {
-								var myDiv = $('<div />').css({
-												height: this.GetHeight(),
-												width: this.GetWidth(),
-												"min-width": "100px",
-												"min-height": "100px"
-											}).attr({
-												id: objId,
-												class: 'with-3d-shadow with-transitions'
-											}).appendTo($(this.Element).empty());
-
-
-								$(document.createElementNS('http://www.w3.org/2000/svg','svg')).css({
-												height: this.GetHeight(),
-												width: this.GetWidth()}).appendTo(myDiv);
+						Qv.AddExtension(_extension,
+							function () {
+								var showOthers = ((this.Layout.Text0.text.toString() * 1) > 0);
+								// load css file
+								Qva.LoadCSS((_webview ? _path : _pathLong) + 'css/nv.d3.min.css');
 								
-								// sizing problem in browser
-								// d3.select('#'+objId).append('svg');
-								
-								// get key elements in QlikView order
-								var listKey = [];
-								$.each(this.Data.Rows, function( index, row ) {
-									if ($.inArray(row[0].text, listKey) === -1) {
-										if (showOthers || row[0].text !== "Others")
-											listKey.push(row[0].text);
-									}
-								});
+								// need a unique id to render chart
+								var objId = this.Layout.ObjectId.replace("\\", "_"); // chart name in CSS class (eg "QvFrame Document_CH01")
 
-								// Transform data set
-								var data = d3.nest()
-											.key(function(d) { return d.key; }).sortKeys(function(a,b) { return listKey.indexOf(a) - listKey.indexOf(b); })
-											.entries(this.Data.Rows.filter(function(row){ return (showOthers || row[0].text !== "Others"); }).map(function(row){
-												return {"key" : row[0].text, "x" : (parseInt(row[1].text)  - 25569)*86400*1000, "y" : parseFloat(row[2].data)}
-											}))
-											.map(function(k){
-												return {"key": k.key, "values": k.values.map(function(v){ return {"x":v.x,"y":v.y} })}
-											});
+								//console.log(objId);
+								if (this.Data.Rows.length > 0) {
+									var myDiv = $('<div />').css({
+													height: this.GetHeight(),
+													width: this.GetWidth(),
+													"min-width": "100px",
+													"min-height": "100px"
+												}).attr({
+													id: objId,
+													class: 'with-3d-shadow with-transitions'
+												}).appendTo($(this.Element).empty());
 
-								var chart;
-								nv.addGraph(function() {
-									chart = nv.models.lineWithFocusChart();
 
-								 // chart.transitionDuration(500);
-									chart.xAxis
-									  .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
-									chart.x2Axis
-									  .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
+									// $(document.createElementNS('http://www.w3.org/2000/svg','svg')).css({
+													// height: this.GetHeight(),
+													// width: this.GetWidth()}).appendTo(myDiv);
+									
+									// sizing problem in browser
+									d3.select('#'+objId).append('svg');
+									
+									// get key elements in QlikView order
+									var listKey = [];
+									$.each(this.Data.Rows, function( index, row ) {
+										if ($.inArray(row[0].text, listKey) === -1) {
+											if (showOthers || row[0].text !== "Others")
+												listKey.push(row[0].text);
+										}
+									});
 
-									chart.yAxis
-									  .tickFormat(d3.format(',.2f'));
-									chart.y2Axis
-									  .tickFormat(d3.format(',.2f'));
+									// Transform data set
+									var data = d3.nest()
+												.key(function(d) { return d.key; }).sortKeys(function(a,b) { return listKey.indexOf(a) - listKey.indexOf(b); })
+												.entries(this.Data.Rows.filter(function(row){ return (showOthers || row[0].text !== "Others"); }).map(function(row){
+													return {"key" : row[0].text, "x" : (parseInt(row[1].text)  - 25569)*86400*1000, "y" : parseFloat(row[2].data)}
+												}))
+												.map(function(k){
+													return {"key": k.key, "values": k.values.map(function(v){ return {"x":v.x,"y":v.y} })}
+												});
 
-									d3.select('#'+objId+' svg')
-									  .datum(data)
-									  .call(chart);
+									var chart;
+									nv.addGraph(function() {
+										chart = nv.models.lineWithFocusChart();
 
-									nv.utils.windowResize(chart.update);
+									 // chart.transitionDuration(500);
+										chart.xAxis
+										  .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
+										chart.x2Axis
+										  .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
 
-								  return chart;
-								});
-																				
-							} else {
-								this.Element.html('<p align="center"><b>No resulting rows to display..</b></p>');
-							}
-						});
+										chart.yAxis
+										  .tickFormat(d3.format(',.2f'));
+										chart.y2Axis
+										  .tickFormat(d3.format(',.2f'));
+
+										d3.select('#'+objId+' svg')
+										  .datum(data)
+										  .call(chart);
+
+										nv.utils.windowResize(chart.update);
+
+									  return chart;
+									});
+																					
+								} else {
+									this.Element.html('<p align="center"><b>No resulting rows to display..</b></p>');
+								}
+							});
+				});
 			});
 		});
 })(jQuery);
